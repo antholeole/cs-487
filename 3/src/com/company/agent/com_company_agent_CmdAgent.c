27d6669db5a137ae3d5c529e39de5bfd70204f83
Anthony Oleinik <antholeinik@gmail.com>
2022-10-31 21:53:19 -0500

    blah

--- 3/src/com/company/agent/com_company_agent_CmdAgent.c
+++ 3/src/com/company/agent/com_company_agent_CmdAgent.c
@@ -1,9 +1,15 @@
 #include <jni.h>
 
 JNIEXPORT jobject JNICALL Java_com_company_agent_CmdAgent_C_1GetLocalTime(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject this,
         jobject getLocalTime
-    ) {
-    getLocalTime.
+) {
+    int32_t local_time = time(NULL);
+
+    jfieldID validField = env->GetFieldID(getLocalTime, "valid", "I");
+    jfieldID localTimeField = env->GetFieldID(getLocalTime, "localTime", "I");
+
+    env->SetObjectField(getLocalTime, validField, 1);
+    env->SetObjectField(getLocalTime, validField, local_time);
 }
